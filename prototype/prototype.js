c7b35b5cbe0682fde1e364b46b5e0d207453cda3
Stefan O'Rear <stefanor@cox.net>
2015-11-06 22:08:10 -0800

    Basic replication

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -63,6 +63,8 @@ class State {
     this._pending = []
     this._commitTimer = 0
     this._commitPromise = null
+
+    this._peers = {}
   }
 
   get id () {
@@ -71,10 +73,20 @@ class State {
 
   _commit () {
     this._epoch++
+    let change = []
     this._pending.forEach(pend => {
       this._data[pend.key] = this._data[pend.key] || {}
+      if (!this._data[pend.key][pend.value]) change.push(pend)
       this._data[pend.key][pend.value] = true
     })
+    if (change.length) {
+      Object.keys(this._peers).forEach(port => {
+        if (this._peers[port].valid) {
+          change.forEach(pend => this._peers[port].sendqueue.push(pend))
+          this._checkSendUpdate(port)
+        }
+      })
+    }
     this._pending = []
     this._commitTimer = 0
     return null
@@ -88,6 +100,83 @@ class State {
       })
     }
     return this._commitPromise
+  }
+
+  rpc_hello (args) {
+    let port = args.port
+    if (this._peers[port]) {
+      throw new Error('Already peered')
+    }
+
+    this._peers[port] = {
+      valid: true,
+      port: port,
+      sending: false,
+      sentepoch: 0,
+      sendqueue: this._dumpAll()
+    }
+
+    this._checkSendUpdate(port)
+    return null
+  }
+
+  rpc_connect (args) {
+    let port = args.port
+    if (this._peers[port]) {
+      throw new Error('Already peered')
+    }
+
+    this._peers[port] = {
+      valid: false,
+      port: port,
+      sending: false,
+      sentepoch: 0,
+      sendqueue: this._dumpAll()
+    }
+
+    return rpcCall(port, 'hello', { port: this.id }).then(() => {
+      this._peers[port].valid = true
+      this._checkSendUpdate(port)
+      return null
+    }, e => {
+      delete this._peers[port]
+      throw e
+    })
+  }
+
+  _dumpAll () {
+    let outp = []
+    Object.keys(this._data).forEach(key => {
+      Object.keys(this._data[key]).forEach(value => {
+        outp.push({ key: key, value: value })
+      })
+    })
+    return outp
+  }
+
+  _checkSendUpdate (port) {
+    let peer = this._peers[port]
+    if (!peer || !peer.valid || peer.sending || !peer.sendqueue.length) return
+    let queue = peer.sendqueue
+    let from_epoch = peer.sentepoch
+
+    peer.sending = true
+    peer.sendqueue = []
+    peer.sentepoch = this._epoch
+
+    rpcCall(port, 'update', {
+      from_epoch: from_epoch, to_epoch: this._epoch, data: queue
+    }).then(() => {
+      peer.sending = false
+      this._checkSendUpdate(port)
+    })
+  }
+
+  rpc_update (args) {
+    args.data.forEach(item => {
+      this._pending.push({ key: String(item.key), value: String(item.value) })
+    })
+    return this._commitSoon()
   }
 
   rpc_get (args) {
