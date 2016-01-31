52a7c30521a64093fc73c6bbe59f85f28a4e3856
Stefan O'Rear <stefanor@cox.net>
2016-01-30 23:56:32 -0800

    start sketching keyspace layout...

--- unbase/src/lib.rs
+++ unbase/src/lib.rs
@@ -1,3 +1,24 @@
+extern crate lmdb;
+
+use lmdb;
+
+// for the second pass, our data is stored in an LMDB object
+// LMDB keys:
+// #1 : general bookkeeping data
+// #1 #1 downstream-id #1 counter : pending messages
+// something about subtree subscriptions?
+// #2 : data per object that is tracked in the system
+// #2 object-id : upstream data (must exist, we are an inclusive cache), our version if different
+// individual subscriptions go here?
+// #3 : data per index (upstream or downstream?? not sure)
+
+// TODO: why does MdbValue exist?  I'd just make it a rust slice
+// TODO: lmdb-rs uses trait objects too much
+
+struct UnbaseEngine {
+    env: lmdb::Environment,
+}
+
 #[test]
 fn it_works() {
 }
