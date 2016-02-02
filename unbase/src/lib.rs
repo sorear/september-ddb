2f81ebd7a17fc4c5dc21a54faced707a6add1804
Stefan O'Rear <stefanor@cox.net>
2016-02-01 23:48:01 -0800

    checkpoint

--- unbase/src/lib.rs
+++ unbase/src/lib.rs
@@ -1,6 +1,23 @@
 extern crate lmdb;
 extern crate uuid;
-use unbase_capnp;
+extern crate capnp;
+
+mod unbase_capnp {
+    include!(concat!(env!("OUT_DIR"), "/unbase_capnp.rs"));
+}
+use lmdb::{RoTransaction, Transaction};
+
+mod keys {
+    pub const SIGNATURE : u8 = 1;
+    pub const SIGVERSION : u8 = 2;
+    pub const SYSTEM_UUID : u8 = 3;
+    pub const DATABASE_UUID : u8 = 4;
+    pub const SUBSCRIBED_PREFIXES : u8 = 5;
+    pub const STATE_BY_ID : u8 = 6;
+
+    pub const SIGNATURE_VALUE : &'static str = "Unbase/T database";
+    pub const SIGVERSION_VALUE : u32 = 0x10000;
+}
 
 // for the second pass, our data is stored in an LMDB object
 // LMDB keys:
@@ -19,16 +36,48 @@ use unbase_capnp;
 //        \----------------+-> StateDiff-\
 // UpIndex ------------------------------+-> OurIndex
 
+// we should *not* tightly couple CRDT flags to the audit trail
+
 // TODO(suggestion): add as_ro() to RwCursor and RwTransaction ?
+// TODO(suggestion): Iter/IterDup needs docs
+// TODO(suggestion): IterRange?
+// TODO(suggestion): IterDup api is kinda unsafe?
 
 struct UnbaseEngine {
     env: lmdb::Environment,
 }
 
-fn epoch_bump_index(tx: &mut lmdb::RwTransaction) {
+struct ObjectId(Vec<u8>);
+
+enum ObjectState {
+    Exists(Vec<u8>),
+    NotExists,
+    NoData,
 }
 
+struct Error;
+impl From<lmdb::Error> for Error {
+}
+type Result<T> = std::result::Result<T, Error>;
 
+struct ReadContext {
+    tx: lmdb::RoTransaction,
+    db: lmdb::Database,
+}
+
+fn query_state(ctx: &ReadContext, id: &ObjectId) -> Result<ObjectState> {
+    let key = vec![keys::STATE_BY_ID];
+    key.extend(id.0);
+    match try!(tx.get(&ctx.db, &key)) {
+        Some(data) => Ok(ObjectState::Exists(data.to_owned())),
+        None => {
+            // loop over prefixes and query them
+        }
+    }
+}
+
+fn epoch_bump_index(tx: &mut lmdb::RwTransaction) {
+}
 
 #[test]
 fn it_works() {
